729e53f922c4bf31a5608dd6f36f1b51a5e4980f
saoen22 <112106310+saoen22@users.noreply.github.com>
2022-08-26 11:14:43 +0700

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:meetup/helpers/appcolors.dart';
+import 'package:meetup/screens/notification_user.dart';
+import 'package:meetup/screens/profile_user.dart';
+import 'package:meetup/screens/profile_userTest.dart';
+import 'package:meetup/screens/splash_screen.dart';
+import 'package:meetup/screens/testAppbar_Nav.dart';
+import 'package:meetup/screens/user2.dart';
+import 'package:meetup/screens/user_ui.dart';
+import 'package:meetup/screens/welcome/components/body.dart';
+import 'screens/friend.dart';
+import 'package:meetup/screens/profile_user.dart';
+import 'package:meetup/screens/user3.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'MeetUp',
+        theme: ThemeData(
+          primaryColor: AppColors.MAIN_COLOR,
+          scaffoldBackgroundColor: Color.fromARGB(255, 224, 232, 234),
+        ),
+        home: user3());
+  }
+}
